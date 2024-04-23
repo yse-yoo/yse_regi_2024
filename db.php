9c3a1146656341ac8d7ff7b59d38be3f7752528c
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-04-23 12:03:36 +0900

    update

--- db.php
+++ db.php
@@ -10,9 +10,9 @@ $db_password = DB_PASSWORD;
 
 $dsn = "{$db_connection}:dbname={$db_name};host={$db_host};charset=utf8;port={$db_port}";
 try {
-    $this->pdo = new PDO($dsn, $db_user, $db_password);
-    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $pdo = new PDO($dsn, $db_user, $db_password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
     echo "接続失敗: " . $e->getMessage();
     echo $dsn;
--- update.php
+++ update.php
@@ -5,11 +5,12 @@ require_once "db.php";
 // POSTデータを受け取る
 $posts = $_POST;
 // var_dump($posts);
+$price = $posts['price'];
 
 // TODO: 合計金額「sales.price」を保存するSQLを作成
-$sql = "";
+$sql = "INSERT INTO sales (price) VALUES ({$price});";
+var_dump($sql);
 // TODO: SQLを実行
 // TODO: レジ画面に戻る
 
 // header('Location: index.php');
-?>
