e7de7766855a796e361e2afc24b9077c32df9300
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-05-07 13:02:54 +0900

    update

--- sales/index.php
+++ sales/index.php
@@ -3,7 +3,7 @@
 require_once "../db.php";
 
 // 売上一覧をすべて取得
-$sql = "SELECT * FROM sales;";
+$sql = "SELECT * FROM sales ORDER BY created_at DESC;";
 // SQLを実行
 $stmt = $pdo->query($sql);
 
@@ -11,8 +11,8 @@ $stmt = $pdo->query($sql);
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $sales[] = $row;
 }
+$total_sales = array_sum(array_column($sales, 'price'));
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,28 +20,36 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>YSEレジ</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body>
-    <h2>売上一覧</h2>
-    <a href="../">戻る</a>
-    <table>
-        <thead>
+    <h2 class="text-2xl text-center">売上一覧</h2>
+    <div class="p-3">
+        <a href="../">戻る</a>
+    </div>
+    <h3 class="text-xl text-center p-3">
+        <label for="">総売上</label>
+        <span><?= number_format($total_sales) ?></span>
+    </h3>
+    <table class="min-w-full divide-y divide-gray-200">
+        <thead class="bg-gray-50">
             <tr>
-                <th>売上日時</th>
-                <th>売上高</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">売上日時</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">売上高</th>
             </tr>
         </thead>
-        <tbody>
-            <?php foreach($sales as $sale): ?>
-            <tr>
-                <td><?= $sale['created_at'] ?></td>
-                <td><?= $sale['price'] ?></td>
-            </tr>
+        <tbody class="bg-white divide-y divide-gray-200">
+            <?php foreach ($sales as $sale) : ?>
+                <tr>
+                    <td class="px-6 py-4 whitespace-nowrap"><?= $sale['created_at'] ?></td>
+                    <td class="px-6 py-4 whitespace-nowrap"><?= number_format($sale['price']) ?></td>
+                </tr>
             <?php endforeach ?>
         </tbody>
     </table>
+
 </body>
 
 </html>
